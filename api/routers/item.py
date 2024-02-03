1555353037209288e84862e0c28779213be65e70
kei-600th <kei19941005@gmail.com>
2024-02-03 14:37:34 +0900

    レスポンスデータにバリデーションを設定する

--- api/routers/item.py
+++ api/routers/item.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter
 from api.cruds import item as item_cruds
 from api.schemas import item as item_schemas
@@ -6,31 +7,31 @@ from api.schemas import item as item_schemas
 router = APIRouter(prefix="/items", tags=["Items"])
 
 
-@router.get("")
+@router.get("", response_model=list[item_schemas.ItemResponse])
 async def find_all():
     return item_cruds.find_all()
 
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=Optional[item_schemas.ItemResponse])
 async def find_by_id(id: int):
     return item_cruds.find_by_id(id)
 
 
-@router.get("/")
+@router.get("/", response_model=list[item_schemas.ItemResponse])
 async def find_by_name(name: str):
     return item_cruds.find_by_name(name)
 
 
-@router.post("")
+@router.post("", response_model=item_schemas.ItemResponse)
 async def create(item_create: item_schemas.ItemCreate):
     return item_cruds.create(item_create)
 
 
-@router.put("/{id}")
+@router.put("/{id}", response_model=Optional[item_schemas.ItemResponse])
 async def update(id: int, item_update: item_schemas.ItemUpdate):
     return item_cruds.update(id, item_update)
 
 
-@router.delete("/{id}")
+@router.delete("/{id}", response_model=Optional[item_schemas.ItemResponse])
 async def delete(id: int):
     return item_cruds.delete(id)
--- api/schemas/item.py
+++ api/schemas/item.py
@@ -19,3 +19,11 @@ class ItemUpdate(BaseModel):
     price: Optional[int] = Field(gt=0, examples=[10000])
     description: Optional[str] = Field(None, examples=["美品です"])
     status: Optional[ItemStatus] = Field(None, examples=[ItemStatus.SOLD_OUT])
+
+
+class ItemResponse(BaseModel):
+    id: int = Field(gt=0, examples=[1])
+    name: str = Field(min_length=2, max_length=20, examples=["PC"])
+    price: int = Field(gt=0, examples=[10000])
+    description: Optional[str] = Field(None, examples=["美品です"])
+    status: ItemStatus = Field(examples=[ItemStatus.ON_SALE])
