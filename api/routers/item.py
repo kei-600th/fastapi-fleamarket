c7745da075da7c74bc1f04e20d9c43a592ffd022
kei-600th <kei19941005@gmail.com>
2024-02-03 10:45:44 +0900

    リクエストデータ(Post)にバリデーションを設定

--- api/routers/item.py
+++ api/routers/item.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Body
 from api.cruds import item as item_cruds
+from api.schemas import item as item_schemas
+
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
@@ -20,7 +22,7 @@ async def find_by_name(name: str):
 
 
 @router.post("")
-async def create(item_create=Body()):
+async def create(item_create: item_schemas.ItemCreate):
     return item_cruds.create(item_create)
 
 
