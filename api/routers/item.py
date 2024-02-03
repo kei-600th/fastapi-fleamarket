b9ed447f9bc90c6a619db299660c5f36a813bf92
kei-600th <kei19941005@gmail.com>
2024-02-03 15:37:09 +0900

    ステータスコードを明示的に指定する

--- api/routers/item.py
+++ api/routers/item.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Path, Query, HTTPException
+from starlette import status
 from api.cruds import item as item_cruds
 from api.schemas import item as item_schemas
 
@@ -6,12 +7,12 @@ from api.schemas import item as item_schemas
 router = APIRouter(prefix="/items", tags=["Items"])
 
 
-@router.get("", response_model=list[item_schemas.ItemResponse])
+@router.get("", response_model=list[item_schemas.ItemResponse], status_code=status.HTTP_200_OK)
 async def find_all():
     return item_cruds.find_all()
 
 
-@router.get("/{id}", response_model=item_schemas.ItemResponse)
+@router.get("/{id}", response_model=item_schemas.ItemResponse, status_code=status.HTTP_200_OK)
 async def find_by_id(id: int = Path(gt=0)):
     found_item = item_cruds.find_by_id(id)
     if not found_item:
@@ -19,17 +20,17 @@ async def find_by_id(id: int = Path(gt=0)):
     return found_item
 
 
-@router.get("/", response_model=list[item_schemas.ItemResponse])
+@router.get("/", response_model=list[item_schemas.ItemResponse], status_code=status.HTTP_200_OK)
 async def find_by_name(name: str = Query(min_length=2, max_length=20)):
     return item_cruds.find_by_name(name)
 
 
-@router.post("", response_model=item_schemas.ItemResponse)
+@router.post("", response_model=item_schemas.ItemResponse, status_code=status.HTTP_201_CREATED)
 async def create(item_create: item_schemas.ItemCreate):
     return item_cruds.create(item_create)
 
 
-@router.put("/{id}", response_model=item_schemas.ItemResponse)
+@router.put("/{id}", response_model=item_schemas.ItemResponse, status_code=status.HTTP_200_OK)
 async def update(item_update: item_schemas.ItemUpdate, id: int = Path(gt=0)):
     updated_item = item_cruds.update(id, item_update)
     if not updated_item:
@@ -37,7 +38,7 @@ async def update(item_update: item_schemas.ItemUpdate, id: int = Path(gt=0)):
     return updated_item
 
 
-@router.delete("/{id}", response_model=item_schemas.ItemResponse)
+@router.delete("/{id}", response_model=item_schemas.ItemResponse, status_code=status.HTTP_200_OK)
 async def delete(id: int = Path(gt=0)):
     deleted_item = item_cruds.delete(id)
     if not deleted_item:
